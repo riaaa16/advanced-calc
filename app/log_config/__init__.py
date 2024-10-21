30c783d2967e7ec84edab9a1aed77b0bfd439cbd
Ria <vg435@njit.edu>
2024-10-21 11:27:32 -0400

    changed from class to function

--- app/log_config/__init__.py
+++ app/log_config/__init__.py
@@ -1,21 +1,27 @@
-# logging_config.py
+"""
+Logging Configuration
+- Sets up logging configuration.
+"""
+
 import logging
 
-# ==============================================================================
-# SETUP LOGGING CONFIGURATION
-# ==============================================================================
 
-# Configure the logging settings for the application.
-# Logging is crucial for tracking events and diagnosing issues in applications.
-class LoggerConfig:
-    @staticmethod
-    def setup_logging():
-        logging.basicConfig(
-            filename='calculator.log', # Specifies the file to write log messages to.
-            level=logging.DEBUG, # Sets the logging level to DEBUG; captures all levels (DEBUG, INFO, WARNING, ERROR, CRITICAL).
-            format='%(asctime)s - %(levelname)s - %(message)s' # Defines the format of the log messages.
-            # Format placeholders:
-            # %(asctime)s - Timestamp of the log entry.
-            # %(levelname)s - Severity level of the log message.
-            # %(message)s - The actual log message.
-        )
+def setup_logging():
+    """
+    Set up the logging configuration.
+
+    This function configures the logging settings for the application, 
+    including the log file name, logging level, and message format.
+    
+    Logs are written to 'calculator.log' and the logging level is set 
+    to DEBUG to capture all levels of log messages.
+    """
+    logging.basicConfig(
+        filename='calculator.log',  # Specifies the file to write log messages to.
+        level=logging.DEBUG,  # Sets the logging level to DEBUG; captures all levels.
+        format='%(asctime)s - %(levelname)s - %(message)s'  # Formats log messages.
+        # Format placeholders:
+        # %(asctime)s - Timestamp of the log entry.
+        # %(levelname)s - Severity level of the log message.
+        # %(message)s - The actual log message.
+    )
